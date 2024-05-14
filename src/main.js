d9ee6ca3e0f077a8fc1dda5d1bf4c79eba3ab4bd
dairen <djean032@gmail.com>
2024-05-14 08:15:30 -0400

    added switching

--- src/main.js
+++ src/main.js
@@ -1,2 +1,46 @@
-const { invoke } = window.__TAURI__.tauri;
+const { invoke } = window.__TAURI__.tauri
 
+function filter_selection(c) {
+    var x, i
+    x = document.getElementsByClassName('filter')
+    for (i = 0; i < x.length; i++) {
+        add_class(x[i], 'hidden')
+        if (x[i].className.indexOf(c) > -1) {
+            remove_class(x[i], 'hidden')
+        }
+    }
+}
+
+function reset_filter() {
+    var x, i
+    x = document.getElementsByClassName('filter')
+    for (i = 0; i < x.length; i++) {
+        add_class(x[i], 'hidden')
+        if (x[i].className.indexOf('home') > -1) {
+            remove_class(x[i], 'hidden')
+        }
+    }
+}
+
+function add_class(element, name) {
+    var i, arr1, arr2
+    arr1 = element.className.split(' ')
+    arr2 = name.split(' ')
+    for (i = 0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += ' ' + arr2[i]
+        }
+    }
+}
+
+function remove_class(element, name) {
+    var i, arr1, arr2
+    arr1 = element.className.split(' ')
+    arr2 = name.split(' ')
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1)
+        }
+    }
+    element.className = arr1.join(' ')
+}
